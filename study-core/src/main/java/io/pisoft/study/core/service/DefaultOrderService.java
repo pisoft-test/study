93c36aa32c1d59da8316cdc3ba983317aa08afe0
keepsmileeverydays <939878065@qq.com>
2017-03-29 10:23:13 +0800

    123

--- study-core/src/main/java/io/pisoft/study/core/service/DefaultOrderService.java
+++ study-core/src/main/java/io/pisoft/study/core/service/DefaultOrderService.java
@@ -1,19 +1,30 @@
 package io.pisoft.study.core.service;
 
+import java.util.Date;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import io.pisoft.study.core.entities.Order;
+import io.pisoft.study.core.entities.Status;
 
 @Service
 @Transactional
 public class DefaultOrderService implements OrderService {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(DefaultOrderService.class);
+
 	@Override
 	public void createOrder(Order order) {
-		// TODO Auto-generated method stub
-
+		order.setCreateTime(new Date());
+		UUID number = UUID.randomUUID();
+		LOGGER.info("uuid is : {}", number);
+		// order.setNumber(number);
 	}
 
 }
--- study-core/src/test/java/io/pisoft/study/core/service/DefaultOrderServiceTest.java
+++ study-core/src/test/java/io/pisoft/study/core/service/DefaultOrderServiceTest.java
@@ -1,11 +1,23 @@
 package io.pisoft.study.core.service;
 
+import io.pisoft.study.core.entities.Order;
+import io.pisoft.study.core.entities.Status;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
 public class DefaultOrderServiceTest extends CoreTest {
-	
+
+	@Autowired
+	private OrderService orderService;
+
 	@Test
-	public void test() {
-		
+	public void testCreateOrder() {
+		Order order = new Order();
+		orderService.createOrder(order);
+		assert order.getCreateTime() != null;
+		assert order.getId() != null;
+		assert order.getNumber() != null;
+		assert order.getStatus().equals(Status.SUBMIT);
 	}
 }
